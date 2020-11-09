fbf80767fcdb04598c3e91a31b077cb6f5ab7ad1
StefanoPruna <48112000+StefanoPruna@users.noreply.github.com>
2020-11-09 15:52:53 +1100

    Python 3
    First Python lecture

---
+++ PythonPractice.py
@@ -0,0 +1,13 @@
+pi = 3.14159
+print(pi)
+test = 'hello'
+testTwo = 'hello'*2
+print(testTwo)
+age = input('Enter your age: ')
+print('Your age is: ' + age)
+ageInYears = input('Enter your date of birth: ')
+ageInYears = int(ageInYears)
+type(ageInYears)
+ageInDays = ageInYears * 365
+print('Your age is :', ageInDays, 'days old') #/ in Python we don't use the + symbol, because
+                                                #/It will use it as an actual sum
