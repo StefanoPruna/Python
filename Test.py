fbf062557e69f3a79f450c6ba7a20850aa4b7bd8
StefanoPruna <48112000+StefanoPruna@users.noreply.github.com>
2020-12-03 17:58:30 +1100

    Assignment 2
    Finished, however it asks for one banch

---
+++ Test.py
@@ -0,0 +1,57 @@
+branches = 0
+bookCategories = {}
+numbOfBranches = []
+
+def countTotal():
+    total = sum(bookCategories.values())
+    average = float(total / 5)
+    print("The Books per category are: " + str(bookCategories) + ";")
+    print("The total amount of books are: " + str(total) + ";")
+    print("And the average of books is: " + str(average))
+
+def checkBranches():
+    while True:
+        try:
+            branches = int(input("Enter the number of branches of the main library: "))
+            assert branches > 0 #Check if it's at least 1
+        except (ValueError, TypeError): #Check if the input is not a number
+            print("Number of branches must be an integer: ")
+        except AssertionError: #Check if the input is 0 or negative
+            print("Number of branches must be at least 1: ")
+        else:
+            print("The branches are in total: " + str(branches))
+            break
+    increase = 0
+    while branches > 0:
+        numbOfBranches.append(increase + 1)
+        branches -= 1
+        increase += 1
+        print(numbOfBranches)
+
+
+def countBook():
+    checkBranches()
+    while True:
+        try:
+            for i in numbOfBranches:
+                computer = int(input("Enter the number of books for Computer of branch  : "))
+                physics = int(input("Enter the number of books for Physics of branch: " ))
+                chemistry = int(input("Enter the number of books for Chemistry of branch: "))
+                biology = int(input("Enter the number of books for Biology of branch "))
+                arts = int(input("Enter the number of books for Arts of branch: "))    
+                bookCategories = {"Computer": computer, "Physics": physics, "Chemistry": chemistry, "Biology": biology, "Arts": arts} 
+                assert computer > 0
+                assert physics > 0
+                assert chemistry > 0
+                assert biology > 0
+                assert arts > 0
+        except (ValueError, TypeError):
+            print("Number of books must be an integer: ")
+        except AssertionError:
+            print("Number of books must be non-negative: ")
+        else:
+            countTotal()
+            break
+
+
+countBook()
--- onlinelibrary.py
+++ onlinelibrary.py
@@ -1,4 +1,13 @@
-""" Function 1: Asking the question until the input isn't: 0, a negative or not a number """
+""" Function 1: Calculate the total amount of books and the average number of books per category"""
+def countTotal():
+    total = sum(bookCategories.values())
+    average = float(total / 5)
+    print("The Books per category are: " + str(bookCategories) + ";")
+    print("The total amount of books are: " + str(total) + ";")
+    print("And the average of books is: " + str(average))
+
+
+""" Function 2: Asking the question until the input isn't: 0, a negative or not a number """
 def checkBranches():
     while True:
         try:
@@ -9,57 +18,32 @@ def checkBranches():
         except AssertionError: #Check if the input is 0 or negative
             print("Number of branches must be at least 1: ")
         else:
-            return branches
+            print("The branches are in total: " + str(branches))
             break
+    
 
-#Fucntion 2: Ask for the number of books for each category for each branch 
-def checkBooks():
-    while True:
-        try:
-            computer = int(input("Enter the number of books for Computer of branch " + nBranch + ":"))
-            physics = int(input("Enter the number of books for Physics of branch " + nBranch + ":" ))
-            chemistry = int(input("Enter the number of books for Chemistry of branch " + nBranch + ": "))
-            biology = int(input("Enter the number of books for Biology of branch " + nBranch + ": "))
-            arts = int(input("Enter the number of books for Arts of branch " + nBranch + ": "))    
-            bookCategories = {"Computer": computer, "Physics": physics, "Chemistry": chemistry, "Biology": biology, "Arts": arts} 
-            assert computer > 0
-            assert physics > 0
-            assert chemistry > 0
-            assert biology > 0
-            assert arts > 0
-        except (ValueError, TypeError):
-            print("Number of books must be an integer: ")
-        except AssertionError:
-            print("Number of books must be non-negative: ")
-        else:
-            return bookCategories
-            break
-
-def checkTotalBooks():
-    nBranch = 1
-    checkBranches()
-    checkBooks()
-    while True:
-        for book in bookCategories:
-            if nBranch in bookCategories:
-                nBranch += 1
-                checkBooks()
-            else:
-                total = sum(bookCategories.values())
-                print(branches, total, bookCategories)
-                break
-
-checkTotalBooks()
-        
-        # Alternative way to sum the total
-        # result = {}
-        # #bookCategories = ["Computer", "Physics", "Chemistry", "Biology", "Arts"]
-        # for book in bookCategories:
-        #     if book in bookCategories:
-        #         result[book] = result.get(countTotal, 0) + 1
-        # return result
-       
-
-
-
-
+""" 
+Fucntion 3: Ask for the number of books for each category for each branch 
+Note: I'm not able to make it loop, I cannot find a solution, it only asks once
+"""    
+checkBranches()
+while True:
+    try:
+        computer = int(input("Enter the number of books for Computer of branch: "))
+        physics = int(input("Enter the number of books for Physics of branch: " ))
+        chemistry = int(input("Enter the number of books for Chemistry of branch: "))
+        biology = int(input("Enter the number of books for Biology of branch "))
+        arts = int(input("Enter the number of books for Arts of branch: "))    
+        bookCategories = {"Computer": computer, "Physics": physics, "Chemistry": chemistry, "Biology": biology, "Arts": arts} 
+        assert computer > 0
+        assert physics > 0
+        assert chemistry > 0
+        assert biology > 0
+        assert arts > 0
+    except (ValueError, TypeError):
+        print("Number of books must be an integer: ")
+    except AssertionError:
+        print("Number of books must be non-negative: ")
+    else:
+        countTotal()
+        break
